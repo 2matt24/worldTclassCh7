9190c78012557423470c41e5c2c40f9cf7df778f
2matt24 <maxbg.do284@gmail.com>
2025-10-23 14:46:12 -0400

    Initial Commit

---
+++ world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  world
+//
+//  Created by Amath Benoit Gaye on 10/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
